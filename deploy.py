18071fd32c71bffcabd3d83b8fdb269283b62c5c
ymiao <ymiao@linx-info.com>
2020-08-06 18:14:34 +0800

    travis test

--- deploy.py
+++ deploy.py
@@ -8,8 +8,9 @@ url = 'https://api.github.com/repos/Caffreyfans/IRbaby-firmware/releases/latest'
 response = requests.get(url)
 data = json.loads(response.text)
 download_url = data['assets'][0]['browser_download_url']
-os.system('wget ' + download_url + ' -P IRbaby_deploy_tmp')
+os.system('wget ' + download_url)
 os.system('unzip -d IRbaby_deploy_tmp IRbaby.zip')
+os.system('rm IRbaby.zip')
 url = 'https://api.github.com/repos/Caffreyfans/IRbaby-android/releases/latest'
 response = requests.get(url)
 data = json.loads(response.text)
