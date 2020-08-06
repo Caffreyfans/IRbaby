db64dfa9a7ae82225e1065d12821cd48b9d395b7
ymiao <ymiao@linx-info.com>
2020-08-06 18:09:02 +0800

    travis test

--- deploy.py
+++ deploy.py
@@ -2,12 +2,14 @@
 import requests
 import json
 import os
+os.system('rm -r IRbaby_deploy_tmp')
 os.system('mkdir -p IRbaby_deploy_tmp')
 url = 'https://api.github.com/repos/Caffreyfans/IRbaby-firmware/releases/latest'
 response = requests.get(url)
 data = json.loads(response.text)
 download_url = data['assets'][0]['browser_download_url']
 os.system('wget ' + download_url + ' -P IRbaby_deploy_tmp')
+os.system('unzip -d IRbaby_deploy_tmp IRbaby.zip')
 url = 'https://api.github.com/repos/Caffreyfans/IRbaby-android/releases/latest'
 response = requests.get(url)
 data = json.loads(response.text)
